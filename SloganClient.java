5eb8dcd3eddbe94793b6da98daa409bf58ae7563
Ryans63 <54951373+Ryans63@users.noreply.github.com>
2020-01-14 20:38:37 -0800

    Update SloganClient.java

--- SloganClient.java
+++ SloganClient.java
@@ -1,4 +1,13 @@
 public class SloganClient{
-  obj = new Slogan(str "")
+  obj = new Slogan(str "Straight cash homie");
   System.out.println(obj);
+  
+  obj2 = new Slogan(str "Cash rules everything around me");
+  System.out.println(obj2);
+  
+  obj3 = new Slogan(str "Be the one"):
+  System.out.println(obj3);
+  
+  obj4 = new Slogan(str "Free the goat");
+  System.out.println(obj4);
 }
